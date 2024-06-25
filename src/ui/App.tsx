3c1ab567995c4d683c866124781cde33ab51f186
Edward Nguyen <nhanna@northstudio.vn>
2024-06-25 14:29:48 +0700

    :sparkles: antd & channels list

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,5 +1,45 @@
+import {Button, Card, Collapse, ConfigProvider, List, Typography} from "antd";
+import useApi from "./lib/hooks/useApi";
+import apiService from "./lib/services/api.service";
+import React, {useEffect} from "react";
+import {Channel, NonThreadGuildBasedChannel} from "discord.js";
+
 export default function App() {
-  return <div>
-    Somethingsfds
-  </div>
+  const [getChannels, {data: channels}] = useApi<NonThreadGuildBasedChannel[]>(apiService.getChannels, apiService, []);
+
+  useEffect(() => {
+    getChannels().then(() => {
+    });
+  }, []);
+
+  return <ConfigProvider theme={{
+    token: {
+      fontFamily: 'Lexend Deca'
+    },
+  }}>
+    <div style={{padding: 16}}>
+      <Typography.Title level={4}>
+        Channels
+      </Typography.Title>
+      <Collapse
+        items={channels.filter(item => !item.parentId).map(channel => ({
+          key: channel.id,
+          label: channel.name,
+          style: {padding: 0},
+          children: <List
+            dataSource={channels.filter(item => item.parentId === channel.id)}
+            renderItem={(item) => {
+              return <List.Item>
+                <List.Item.Meta
+                  title={item.name}
+                />
+                <Button size={'small'}>Bind Project</Button>
+              </List.Item>
+            }}
+          />
+        }))}
+        defaultActiveKey={['1']}
+      />
+    </div>
+  </ConfigProvider>
 }
--- src/ui/lib/services/api.service.ts
+++ src/ui/lib/services/api.service.ts
@@ -1,7 +1,39 @@
-class ApiClient {
-  call(endpoint: string) {
+import axios, {AxiosRequestConfig} from "axios";
 
+class ApiService {
+  axiosInstance = axios.create({
+    baseURL: '/api',
+  });
+  auth = {
+    accessToken: '',
+    refreshToken: '',
+  }
+
+  getChannels() {
+    return this.callApi('GET', '/channels');
+  }
+
+  async callApi(method: string, endpoint: string, data: any = {}, config?: AxiosRequestConfig, ignoreAuth = false) {
+    try {
+      const r = await this.axiosInstance({
+        method,
+        url: endpoint,
+        data,
+        headers: {
+          Authorization: this.auth.accessToken ? `Bearer ${this.auth.accessToken}` : undefined,
+        },
+        ...config,
+      });
+      return r.data;
+    } catch (e) {
+      if (e.response) {
+        if (e.response.data) throw e.response.data;
+        throw e.response;
+      } else {
+        throw e;
+      }
+    }
   }
 }
 
-export default new ApiClient();
+export default new ApiService();
